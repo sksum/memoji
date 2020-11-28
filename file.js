2d95e1d1dfa13ef346757c9460540948434f2a46
sksum <sakshammrigdgr8@gmail.com>
2020-11-28 18:57:08 +0530

    checkbos

--- file.js
+++ file.js
@@ -61,7 +61,7 @@ async function renderPrediction() {
     console.log("still here")
     predictions.forEach(prediction => {
       const keypoints = prediction.scaledMesh;
-       if (false) { // stat,triangulate
+       if (document.getElementById("tria").checked) { // stat,triangulate
         ctx.strokeStyle = GREEN;
         ctx.lineWidth = 2.5;
 
@@ -157,47 +157,6 @@ async function importLib  () {
     console.log(model)
     const video = document.getElementById("now");
       renderPrediction();
-    // const predictions = await model.estimateFaces({ input: video });
-    // if (predictions.length > 0) {
-    //     /*
-    //     `predictions` is an array of objects describing each detected face, for example:
-    
-    //     [
-    //       {
-    //         faceInViewConfidence: 1, // The probability of a face being present.
-    //         boundingBox: { // The bounding box surrounding the face.
-    //           topLeft: [232.28, 145.26],
-    //           bottomRight: [449.75, 308.36],
-    //         },
-    //         mesh: [ // The 3D coordinates of each facial landmark.
-    //           [92.07, 119.49, -17.54],
-    //           [91.97, 102.52, -30.54],
-    //           ...
-    //         ],
-    //         scaledMesh: [ // The 3D coordinates of each facial landmark, normalized.
-    //           [322.32, 297.58, -17.54],
-    //           [322.18, 263.95, -30.54]
-    //         ],
-    //         annotations: { // Semantic groupings of the `scaledMesh` coordinates.
-    //           silhouette: [
-    //             [326.19, 124.72, -3.82],
-    //             [351.06, 126.30, -3.00],
-    //             ...
-    //           ],
-    //           ...
-    //         }
-    //       }
-    //     ]
-    //     */
-    
-    //     for (let i = 0; i < predictions.length; i++) {
-    //       const keypoints = predictions[i].scaledMesh;
-    
-    //       // Log facial keypoints.
-
-    //       console.log(keypoints.length, keypoints[0][0]);
-    //     }
-    //   }
 }
 var video;
 async function setupCamera (constraints) {
